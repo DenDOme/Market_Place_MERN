ada01b9c1b73afa3802bba21145738735377c616
DenDOme <newlifeofkirim@gmail.com>
2025-02-11 02:04:52 +0700

    added user action save

--- backend/ProductService/src/controllers/userAction.controller.js
+++ backend/ProductService/src/controllers/userAction.controller.js
@@ -1,9 +1,41 @@
-/*
-TODO:
+import Category from '../models/category.model.js';
+import UserAction from '../models/userAction.model.js';
 
-create UserAction :{
-    if( user action exists) add +1
+export const createUserAction = async (req, res) => {
+    const { category } = req.body;
+    const { userId } = req.user;
+
+    try {
+        if(!category) {
+            return res.status(400).json({ message: "category not provided" });
+        }
+
+        const isCategoryExists = await Category.findOne({ category });
+        if(!isCategoryExists) {
+            return res.status(404).json({ message: "Category not found" });
+        }
+
+        const isUserActionExists = await UserAction.findOne({ userId, category });
+
+        if(!isUserActionExists) {
+            const newUserAction = new UserAction({
+                userId,
+                category,
+                quantity: 1
+            });
+
+            await newUserAction.save();
+            return res.status(201).json({ message: "User action created" });
+        }
+
+        else {
+            const ExistingUserAction = await UserAction.findOne({ userId, category });
+            ExistingUserAction.quantity += 1;
+            await ExistingUserAction.save();
+            return res.status(200).json({ message: "User action updated" });
+        }
+    } catch (error) {
+        console.error('Error in createUserAction | userAction controller', error.message);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
 }
-
-getUserActions
-*/ 
